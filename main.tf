7e46169fc2069239aa9ce95f52c6f8b96ecde11e
SamirZNR <118886482+SamirZNR@users.noreply.github.com>
2026-01-12 14:06:09 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-068c0051b15cdb816"
+  instance_type = "t3.micro"
+  tags = {
+    Name = "custom-module"
+  }
+
+}
 
